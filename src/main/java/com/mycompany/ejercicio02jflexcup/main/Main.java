a6b9ba71ccd08ab6075579d25bd99194784929d5
Melanni Tzul <melannitzul@gmail.com>
2022-02-14 11:34:27 -0600

    Ejercicio realizado

---
+++ src/main/java/com/mycompany/ejercicio02jflexcup/main/Main.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.ejercicio02jflexcup.main;
+import com.mycompany.ejercicio02jflexcup.Frontend.ventana;
+
+/**
+ *
+ * @author Melanni Tzul
+ */
+public class Main {
+    public static void main (String[] args){
+        ventana NuevaVentana = new ventana() ;
+        NuevaVentana.setVisible(true);
+    }   
+}
